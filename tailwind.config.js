e4b30d9993886558944abcab9f971e12e4884858
ahmetayyildiz66 <ahmetayyildiz66@gmail.com>
2021-06-07 12:13:16 +0300

    Set font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bright-orange': 'hsl(31, 77%, 52%)',
+        'dark-cyan': 'hsl(184, 100%, 22%)',
+        'very-dark-cyan': 'hsl(179, 100%, 13%)',
+        'transparent-white': 'hsla(0, 0%, 100%, 0.75)',
+        'very-light-gray': 'hsl(0, 0%, 95%)'
+      },
+      fontFamily: {
+        'lexend-deca': ['Lexend Deca', 'sans-serif'],
+        'big-shoulder': ['Big Shoulders Display', 'cursive']
+      }
+    },
+    screens: {
+      'sm': '375px',
+      'lg': '1440px'
+    }
   },
   variants: {
     extend: {},
